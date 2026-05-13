71bf83862f61888e7f7f146206415eb500602b65
bdw605807-ship-it <bdw605807@gmail.com>
2026-05-14 02:33:59 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -42,8 +42,8 @@ const context = (jid, img) => ({
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363225356834044@newsletter',
-        newsletterName: '𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+        newsletterJid: '120363427228734118@newsletter',
+        newsletterName: '𝑨𝑩𝑫𝑶🥷🏼',
         serverMessageId: 0
     },
     externalAdReply: {
